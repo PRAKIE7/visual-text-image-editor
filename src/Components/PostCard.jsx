d009e41110d0be39c34022eddcf83ed574a7724b
PRAKIE7 <prakherbhatt@gmail.com>
2024-05-14 01:48:20 +0530

    Created Postcard and Selectfield.

---
+++ src/Components/PostCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import appwriteService from '../Appwrite/Database'
+import { Link } from 'react-router-dom'
+
+function PostCard({$id, Title, Image}) {
+  return (
+    <div>
+      <Link to={`/post/${$id}`}>
+      <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                <img src={appwriteService.filePreview(Image)} alt={Title}
+                className='rounded-xl' />
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{Title}</h2>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default PostCard
